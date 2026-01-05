6a228fbc5ffda75b4d1f47737c2c64bd8a178794
RangoDisco <diasmaxime1@gmail.com>
2026-01-05 17:20:46 +0100

    feat(anna): added album model

---
+++ thirdpartyAPIs/anna/internal/models/Album.go
@@ -0,0 +1,12 @@
+package models
+
+type Album struct {
+	ID         int64  `json:"id" gorm:"primary_key"`
+	ExternalID string `json:"external_id"`
+	Name       string `json:"name"`
+	// TODO: Enum
+	AlbumType   string   `json:"album_type"`
+	ReleaseDate string   `json:"release_date"`
+	TotalTracks int64    `json:"total_tracks"`
+	Artists     []Artist `json:"artists" gorm:"many2many:artist_albums;"`
+}

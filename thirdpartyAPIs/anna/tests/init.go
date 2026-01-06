88f69815a52273884b9bc3db4b044cae02686a4a
RangoDisco <diasmaxime1@gmail.com>
2026-01-06 20:40:53 +0100

    feat(anna): added test init

---
+++ thirdpartyAPIs/anna/tests/init.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rangodisco/yhar/config"
+	"github.com/rangodisco/yhar/thirdpartyAPIs/anna/config/database"
+	"github.com/rangodisco/yhar/thirdpartyAPIs/anna/config/router"
+)
+
+var Router *gin.Engine
+
+func init() {
+	// Load main environment variables
+	godotenv.Load(".env")
+
+	// Ensure that the environment is set to test
+	os.Setenv("GIN_MODE", "test")
+
+	// Load environment variables
+	config.LoadEnv()
+
+	database.SetupDatabase()
+
+	// Setup router
+	Router = router.SetupRouter()
+}

fc6bc46634a7a3a5f535a28f7869f6623922e9cb
RangoDisco <diasmaxime1@gmail.com>
2026-01-09 20:41:12 +0100

    feat(anna): updated types

--- thirdpartyAPIs/anna/internal/repositories/track.go
+++ thirdpartyAPIs/anna/internal/repositories/track.go
@@ -1,14 +1,14 @@
 package repositories
 
 import (
-	"github.com/rangodisco/yhar/pkg/types/anna/track"
+	"github.com/rangodisco/yhar/pkg/types/anna/scrobble"
 	"github.com/rangodisco/yhar/thirdpartyAPIs/anna/config/database"
 	"github.com/rangodisco/yhar/thirdpartyAPIs/anna/internal/models"
 )
 
-func GetTrackInfoByScrobble(scrobble track.InfoByScrobbleRequest) (*models.Track, error) {
+func FindTrackInfoByScrobble(scrobble scrobble.InfoRequest) (*models.Track, error) {
 	var t models.Track
-	err := database.GetDB().Where("name = ?", scrobble.Title).First(&t).Error
+	err := database.GetDB().Preload("Artists").Where("name = ?", scrobble.Title).Order("popularity").Last(&t).Error
 	if err != nil {
 		return nil, err
 	}

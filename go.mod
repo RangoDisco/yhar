f9ac0e87cf6eadd0ed4b811f72a9fa4caadf8f7b
RangoDisco <diasmaxime1@gmail.com>
2026-01-10 15:05:13 +0100

    feat(anna): updated logger config

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.24.1
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/joho/godotenv v1.5.1
-	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
--- thirdpartyAPIs/anna/config/init.go
+++ thirdpartyAPIs/anna/config/init.go
@@ -19,13 +19,6 @@ func Init() (*http.Server, error) {
 	r := router.SetupRouter()
 	router.LoadRoutes(r)
 
-	err = r.Run()
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("unable to start anna's router: %v", err))
-	}
-
-	SetupLogger()
-
 	return &http.Server{
 		Addr:         ":8081",
 		Handler:      Router(),

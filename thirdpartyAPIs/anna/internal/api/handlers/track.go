137402316667da9c573ed48a8eeb1932d3dc6d65
RangoDisco <diasmaxime1@gmail.com>
2026-01-10 11:16:14 +0100

    feat(anna): handled getting metadatas from scrobble infos

--- thirdpartyAPIs/anna/internal/api/handlers/track.go
+++ thirdpartyAPIs/anna/internal/api/handlers/track.go
@@ -1,5 +1,31 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func GetTrackInfoByScrobble(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/rangodisco/yhar/pkg/types/anna/scrobble"
+	"github.com/rangodisco/yhar/thirdpartyAPIs/anna/internal/services"
+)
+
+func GetTrackInfoByScrobble(c *gin.Context) {
+	var body scrobble.InfoRequest
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	scrobbleInfo, err := services.GetInfoByScrobble(body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{
+			"scrobble": scrobbleInfo,
+		},
+	})
+	return
+}

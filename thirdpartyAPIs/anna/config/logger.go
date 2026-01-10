adaae7bc8db81b51132b7335537d263a42b4c545
RangoDisco <diasmaxime1@gmail.com>
2026-01-10 15:05:09 +0100

    feat(anna): updated logger config

--- thirdpartyAPIs/anna/config/logger.go
+++ thirdpartyAPIs/anna/config/logger.go
@@ -5,19 +5,10 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func SetupLogger() {
-	file := &lumberjack.Logger{
-		Filename:   "anna.log",
-		MaxSize:    100,
-		MaxBackups: 3,
-		MaxAge:     28,
-		Compress:   true,
-	}
-
-	multiWriter := io.MultiWriter(os.Stdout, file)
-	gin.DefaultWriter = multiWriter
-	gin.DefaultErrorWriter = multiWriter
+	f, _ := os.Create("logs/anna.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }

3b056334129ad80fd95f3053899803add0843018
RangoDisco <diasmaxime1@gmail.com>
2026-01-13 20:42:01 +0100

    fix(server): added navidrome router

---
+++ server/config/router.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rangodisco/yhar/server/internal/api/handlers"
+)
+
+func SetupRouter() *gin.Engine {
+	SetupLogger()
+	r := gin.New()
+	loadRoutes(r)
+
+	// TOOD: middleware
+	return r
+}
+
+func loadRoutes(r *gin.Engine) {
+	api := r.Group("/api")
+	navidrome := api.Group("/navidrome")
+	navidrome.GET("/getNowPlaying", handlers.GetNowPlaying)
+}

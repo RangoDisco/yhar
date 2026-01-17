13417c63b2ac9da11335b84cce8948a2f16cfbdc
RangoDisco <diasmaxime1@gmail.com>
2026-01-17 21:56:50 +0100

    fix(metadata): updated db env variables

--- internal/metadata/config/database/database.go
+++ internal/metadata/config/database/database.go
@@ -26,11 +26,11 @@ func SetupDatabase() error {
 
 func InitDatabase() error {
 	// Open a database connection
-	name := os.Getenv("DB_NAME")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
+	name := os.Getenv("META_DB_NAME")
+	user := os.Getenv("META_DB_USER")
+	password := os.Getenv("META_DB_PASSWORD")
+	host := os.Getenv("META_DB_HOST")
+	port := os.Getenv("META_DB_PORT")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		host, user, password, name, port)

77b0211a8e7ad8358e173164fc8b5f1fb4cc479e
RangoDisco <diasmaxime1@gmail.com>
2026-01-17 14:58:28 +0100

    feat(api): added user model

---
+++ internal/api/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	Timestamps
+	ID         int64      `json:"id" gorm:"primary_key;autoIncrement"`
+	Username   string     `json:"username" gorm:"type:varchar(75);not null"`
+	Origin     string     `json:"origin" gorm:"type:varchar(255);not null"`
+	ExternalID string     `json:"external_id" gorm:"type:varchar(255);not null"`
+	Scrobbles  []Scrobble `json:"scrobbles" gorm:"foreignkey:UserID;"`
+}

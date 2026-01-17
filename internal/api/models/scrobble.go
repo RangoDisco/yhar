aab5c7035bff08c0902c0b6f99525100cc8124cb
RangoDisco <diasmaxime1@gmail.com>
2026-01-17 14:58:15 +0100

    feat(api): added scrobble model

---
+++ internal/api/models/scrobble.go
@@ -0,0 +1,27 @@
+package models
+
+import "database/sql/driver"
+
+type scrobbleOrigin string
+
+const (
+	SUBSONIC albumType = "SUBSONIC"
+)
+
+func (so *scrobbleOrigin) Scan(value interface{}) error {
+	*so = scrobbleOrigin(value.([]byte))
+	return nil
+}
+
+func (so scrobbleOrigin) Value() (driver.Value, error) {
+	return string(so), nil
+}
+
+type Scrobble struct {
+	ID      int64          `json:"id" gorm:"primary_key;autoIncrement"`
+	Origin  scrobbleOrigin `json:"origin" gorm:"type:scrobble_origin;not null"`
+	TrackID int64
+	Track   Track `json:"track" gorm:"foreignkey:TrackID;references:ID;"`
+	UserID  int64
+	User    User `json:"user" gorm:"foreignkey:UserID;references:ID;"`
+}

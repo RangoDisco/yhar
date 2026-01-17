81a945469dabf3616442f72c8185bf90e7c170ae
RangoDisco <diasmaxime1@gmail.com>
2026-01-17 14:58:05 +0100

    feat(api): added artist model

---
+++ internal/api/models/artist.go
@@ -0,0 +1,10 @@
+package models
+
+type Artist struct {
+	Timestamps
+	ID     int64   `json:"id" gorm:"primary_key;autoIncrement"`
+	Name   string  `json:"name" gorm:"type:varchar(150);not null"`
+	Genres []Genre `json:"genres" gorm:"many2many:artist_genres;"`
+	Albums []Album `json:"albums" gorm:"many2many:artist_albums;"`
+	Tracks []Track `json:"tracks" gorm:"many2many:track_artists;"`
+}

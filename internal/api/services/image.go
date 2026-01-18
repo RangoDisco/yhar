3742bb76933b5096207e82c4fd830f2725011163
RangoDisco <diasmaxime1@gmail.com>
2026-01-18 12:49:29 +0100

    feat(api): handled image creation

---
+++ internal/api/services/image.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"github.com/rangodisco/yhar/internal/api/models"
+	"github.com/rangodisco/yhar/internal/api/repositories"
+)
+
+// GetOrCreateImage looks for the url in database, if it doesn't exist, creates and returns it
+func GetOrCreateImage(url string) (*models.Image, error) {
+	existingImage, err := repositories.FindActiveImageByUrl(url)
+	if err == nil && existingImage.Url != "" {
+		return existingImage, nil
+	}
+
+	newImage, err := repositories.CreateImage(url)
+	if err != nil {
+		return nil, err
+	}
+	return newImage, nil
+}

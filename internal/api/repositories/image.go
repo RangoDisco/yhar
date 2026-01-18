ed8439c923d24bfada99a471b5a3a9eee1370be9
RangoDisco <diasmaxime1@gmail.com>
2026-01-18 12:49:16 +0100

    feat(api): added image repository

---
+++ internal/api/repositories/image.go
@@ -0,0 +1,27 @@
+package repositories
+
+import (
+	"github.com/rangodisco/yhar/internal/api/config/database"
+	"github.com/rangodisco/yhar/internal/api/models"
+)
+
+func FindActiveImageByUrl(url string) (*models.Image, error) {
+	var i models.Image
+
+	err := database.GetDB().Where("url = ?", url).First(&i).Error
+	if err != nil {
+		return nil, err
+	}
+	return &i, err
+}
+
+func CreateImage(url string) (*models.Image, error) {
+	img := &models.Image{
+		Url: url,
+	}
+	err := database.GetDB().Create(&img).Error
+	if err != nil {
+		return nil, err
+	}
+	return img, err
+}

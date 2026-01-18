bb6f4fc15889799a087a41b83aa8195f91d13af1
RangoDisco <diasmaxime1@gmail.com>
2026-01-18 12:50:27 +0100

    feat(api): added artist repository

---
+++ internal/api/repositories/artist.go
@@ -0,0 +1,25 @@
+package repositories
+
+import (
+	"github.com/rangodisco/yhar/internal/api/config/database"
+	"github.com/rangodisco/yhar/internal/api/models"
+)
+
+func FindActiveArtistByName(name string) (*models.Artist, error) {
+	var a models.Artist
+
+	err := database.GetDB().Where("name = ?", name).First(&a).Error
+	if err != nil {
+		return nil, err
+	}
+	return &a, err
+}
+
+func CreateArtist(artist *models.Artist) (*models.Artist, error) {
+	res := database.GetDB().Create(&artist)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return artist, nil
+}

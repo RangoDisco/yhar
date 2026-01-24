f387941dd1a3fe77d0165c8083018e714371cc0d
RangoDisco <diasmaxime1@gmail.com>
2026-01-24 14:23:05 +0100

    feat(api): handled genre when creating an artist

---
+++ internal/api/repositories/genre.go
@@ -0,0 +1,25 @@
+package repositories
+
+import (
+	"github.com/rangodisco/yhar/internal/api/config/database"
+	"github.com/rangodisco/yhar/internal/api/models"
+)
+
+func FindActiveGenreByName(name string) (*models.Genre, error) {
+	var g models.Genre
+	err := database.GetDB().First(&g, "name = ?", name).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &g, nil
+}
+
+func CreateGenre(g *models.Genre) error {
+	err := database.GetDB().Create(g).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

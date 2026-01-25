f8a5450666445406514331dbc9a2885bcfaf359c
RangoDisco <diasmaxime1@gmail.com>
2026-01-25 17:35:51 +0100

    feat(user): added unique on ExternalID

--- internal/api/models/user.go
+++ internal/api/models/user.go
@@ -5,6 +5,6 @@ type User struct {
 	ID         int64      `json:"id" gorm:"primary_key;autoIncrement"`
 	Username   string     `json:"username" gorm:"type:varchar(75);not null"`
 	Origin     string     `json:"origin" gorm:"type:varchar(255);not null"`
-	ExternalID string     `json:"external_id" gorm:"type:varchar(255);not null"`
+	ExternalID string     `json:"external_id" gorm:"type:varchar(255);not null;uniqueIndex"`
 	Scrobbles  []Scrobble `json:"scrobbles" gorm:"foreignkey:UserID;"`
 }

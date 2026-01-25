ebf8e654115ea9e25e9ab1045abee61a895563f0
RangoDisco <diasmaxime1@gmail.com>
2026-01-25 17:00:57 +0100

    fix(track): allowed null value on musicbrainz id

--- internal/api/models/track.go
+++ internal/api/models/track.go
@@ -7,5 +7,5 @@ type Track struct {
 	Artists       []Artist `json:"artists" gorm:"many2many:track_artists;"`
 	AlbumID       int64
 	Album         Album  `json:"album" gorm:"foreignkey:AlbumID;references:ID;default:null"`
-	MusicBrainzID string `json:"music_brainz_id" gorm:"type:varchar(255);not null"`
+	MusicBrainzID string `json:"music_brainz_id" gorm:"type:varchar(255);default:null"`
 }

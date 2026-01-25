a42a45b9d926ef0d7cb610ef7ba9564f02e7e35f
RangoDisco <diasmaxime1@gmail.com>
2026-01-25 18:41:00 +0100

    fix(user): handled fetching by username instead of external id

--- internal/api/services/user.go
+++ internal/api/services/user.go
@@ -5,13 +5,13 @@ import (
 	"github.com/rangodisco/yhar/internal/api/repositories"
 )
 
-func GetOrCreateUser(username, id string) (*models.User, error) {
-	existingUser, err := repositories.FindActiveUserByExternalID(id)
+func GetOrCreateUser(username string) (*models.User, error) {
+	existingUser, err := repositories.FindActiveByUsername(username)
 	if err == nil {
 		return existingUser, err
 	}
 
-	model := scrobbleUserToUserModel(username, id)
+	model := scrobbleUserToUserModel(username)
 	err = repositories.PersistUser(model)
 	if err != nil {
 		return nil, err
@@ -20,10 +20,9 @@ func GetOrCreateUser(username, id string) (*models.User, error) {
 	return model, nil
 }
 
-func scrobbleUserToUserModel(username, id string) *models.User {
+func scrobbleUserToUserModel(username string) *models.User {
 	return &models.User{
-		Username:   username,
-		ExternalID: id,
+		Username: username,
 		// TODO: handle enum
 		Origin: "SUBSONIC",
 	}

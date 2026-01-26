1b636d0380fe425d47b984878b4505f92e964817
RangoDisco <diasmaxime1@gmail.com>
2026-01-26 08:05:41 +0100

    feat(auth): added role/permissions to user model

--- internal/api/models/user.go
+++ internal/api/models/user.go
@@ -7,4 +7,7 @@ type User struct {
 	Origin     string     `json:"origin" gorm:"type:varchar(255);not null"`
 	ExternalID string     `json:"external_id" gorm:"type:varchar(255);default: null;uniqueIndex"`
 	Scrobbles  []Scrobble `json:"scrobbles" gorm:"foreignkey:UserID;"`
+	Password   string     `json:"password" gorm:"type:varchar(255);not null"`
+	RoleID     int64      `json:"role_id" gorm:"not null"`
+	Role
 }

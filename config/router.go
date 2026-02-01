629722da33a06b15ae135953cc5b18342af7a950
RangoDisco <diasmaxime1@gmail.com>
2026-02-01 12:43:32 +0100

    feat(auth): added user sub group with middleware

--- config/router.go
+++ config/router.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rangodisco/yhar/internal/api/handlers"
+	"github.com/rangodisco/yhar/internal/api/middlewares"
 )
 
 func SetupRouter() *gin.Engine {
@@ -17,11 +18,20 @@ func SetupRouter() *gin.Engine {
 func loadRoutes(r *gin.Engine) {
 	api := r.Group("/api")
 
-	// THIRDPARTY
-	navidrome := api.Group("/navidrome")
-	navidrome.GET("/getNowPlaying", handlers.ManualNowPlayingPoll)
-
 	// AUTH
 	auth := api.Group("/auth")
 	auth.POST("/login", handlers.Login)
+
+	protected := api.Group("/")
+	protected.Use(middlewares.Authenticate())
+
+	// THIRDPARTY
+	navidrome := protected.Group("/navidrome")
+	navidrome.GET("/getNowPlaying", handlers.ManualNowPlayingPoll)
+
+	// USER DATA
+	user := protected.Group("/users/:userID")
+	user.Use(middlewares.CheckUserPrivacy())
+
+	// SCROBBLES INFOS
 }

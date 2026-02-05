0306ff2a1d5722f08b706717d1a2cb58fe51d3a4
RangoDisco <diasmaxime1@gmail.com>
2026-02-05 07:51:08 +0100

    feat(web): ContentCard component

---
+++ web/src/lib/components/top/ContentCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import * as Avatar from "$lib/components/ui/avatar/index";
+
+    let {
+        title,
+        pictureUrl,
+        contentType
+    } = $props();
+
+</script>
+
+<article class="flex flex-col items-center justify-center">
+    <Avatar.Root class="{contentType !== 'artists' ? 'rounded-md' : ''} h-[80%] w-[80%] aspect-square">
+        <Avatar.Image src={pictureUrl}
+                      alt={`${title}'s picture`}/>
+        <Avatar.Fallback>{title}</Avatar.Fallback>
+    </Avatar.Root>
+    <p>{title}</p>
+</article>

f1137e19b663cef8b5cb991f59577c72c47c425a
RangoDisco <diasmaxime1@gmail.com>
2026-02-06 21:36:58 +0100

    feat(web): added list/grid wrapping components

---
+++ web/src/lib/components/top/ContentGridWrapper.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import ContentCard from "$lib/components/top/ContentCard.svelte";
+    import type {Snippet} from "svelte";
+
+    type Props = {
+        title: string;
+        url: string;
+        children: Snippet
+
+    }
+
+    let {
+        title, url, children
+    }: Props = $props();
+</script>
+
+<section class="flex flex-col gap-4">
+    <div class="flex justify-between items-center">
+        <h1 class="text-2xl">Top {title}</h1>
+        <a class="text-sm text-muted-foreground" href={url}>View all</a>
+    </div>
+    {@render children()}
+</section>

c69b0b615fe204f74fbe0161e151a8f300f19080
RangoDisco <diasmaxime1@gmail.com>
2026-02-06 21:36:43 +0100

    feat(web): moved history's components to tracks' folder

---
+++ web/src/lib/components/top/tracks/HistoryList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import HistoryListItem from "$lib/components/top/tracks/HistoryListItem.svelte";
+</script>
+
+<section class="flex flex-col gap-4">
+    <div class="flex justify-between items-center">
+        <h1 class="text-2xl">History</h1>
+        <a class="text-sm text-muted-foreground" href="history">View all</a>
+    </div>
+    <div class="flex flex-col gap-4">
+        <HistoryListItem title="wake up" album="sace6" scrobbledAt="4 minutes ago"
+                         pictureUrl="https://wrapped.nero.rangodisco.eu/cacheimages/4780a4842ce999779dee6863c1e3a0fe"
+        />
+        <HistoryListItem title="Venturing" album="sace6" scrobbledAt="4 minutes ago"
+                         pictureUrl="https://wrapped.nero.rangodisco.eu/cacheimages/ea86e05166610135d84e5724be5a23e1"
+        />
+        <HistoryListItem title="Ptite soeur" album="sace6" scrobbledAt="4 minutes ago"
+                         pictureUrl="https://wrapped.nero.rangodisco.eu/cacheimages/4780a4842ce999779dee6863c1e3a0fe"
+        />
+        <HistoryListItem title="Ptite soeur" album="sace6" scrobbledAt="4 minutes ago"
+                         pictureUrl="https://wrapped.nero.rangodisco.eu/cacheimages/4780a4842ce999779dee6863c1e3a0fe"
+        />
+        <HistoryListItem title="Ptite soeur" album="sace6" scrobbledAt="4 minutes ago"
+                         pictureUrl="https://wrapped.nero.rangodisco.eu/cacheimages/4780a4842ce999779dee6863c1e3a0fe"
+        />
+        <HistoryListItem title="Ptite soeur" album="sace6" scrobbledAt="4 minutes ago"
+                         pictureUrl="https://wrapped.nero.rangodisco.eu/cacheimages/4780a4842ce999779dee6863c1e3a0fe"
+        />
+    </div>
+</section>

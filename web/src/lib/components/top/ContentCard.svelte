f86b646218da808801b31ab9d72aca8c92bdf339
RangoDisco <diasmaxime1@gmail.com>
2026-02-09 18:01:21 +0100

    fix(web): stopped displaying link when no contentID was passed to cards

--- web/src/lib/components/top/ContentCard.svelte
+++ web/src/lib/components/top/ContentCard.svelte
@@ -5,18 +5,25 @@
 
     let {
         title,
-        contentID,
+        contentID = $bindable(null),
         pictureUrl,
         contentType
     } = $props();
 
 </script>
 
-<article class="w-[30%] md:w-[15%]">
-    <a class="flex flex-col items-center justify-center gap-2"
-       href="/users/{page.params.userID}/top/{contentType}/{contentID}">
+<article class="w-[30%] md:w-[15%] flex flex-col items-center justify-center gap-2">
+    {#if contentID}
+        <a class="flex flex-col items-center justify-center gap-2"
+           href="/users/{page.params.userID}/top/{contentType}/{contentID}" disabled>
+            <img class="{contentType !== 'artists' ? 'rounded-md' : 'rounded-full'} w-full aspect-square"
+                 src={pictureUrl}
+                 alt={`${title}'s picture`}/>
+            <p class="text-center mb-auto">{title}</p>
+        </a>
+    {:else}
         <img class="{contentType !== 'artists' ? 'rounded-md' : 'rounded-full'} w-full aspect-square" src={pictureUrl}
              alt={`${title}'s picture`}/>
         <p class="text-center mb-auto">{title}</p>
-    </a>
+    {/if}
 </article>

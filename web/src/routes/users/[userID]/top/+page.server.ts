25fa3042b16de1679962ed6aca41b254cdf04921
RangoDisco <diasmaxime1@gmail.com>
2026-02-09 02:07:07 +0100

    refactor(web): updated promise helper

--- web/src/routes/users/[userID]/top/+page.server.ts
+++ web/src/routes/users/[userID]/top/+page.server.ts
@@ -5,7 +5,7 @@ import { fetcher } from '$lib/fetcher';
 export const load: PageServerLoad = async ({ url, params, cookies }) => {
 	const { userID } = params;
 
-	const getStreamedPeriodData = async (period: 'month' | 'year' | 'overall') => ({
+	const getStreamedPeriodData = async (period: 'week' | 'month' | 'year' | 'overall') => ({
 		artists: await fetcher(
 			`${API_URL}/users/${userID}/scrobbles/top/artists?period=${period}&limit=6`,
 			'GET',
@@ -26,37 +26,23 @@ export const load: PageServerLoad = async ({ url, params, cookies }) => {
 		)
 	});
 
-	const [wArtists, wAlbums, wTracks, history] = await Promise.all([
-		fetcher(
-			`${API_URL}/users/${userID}/scrobbles/top/artists?period=week&limit=6`,
-			'GET',
-			null,
-			cookies
-		),
-		fetcher(
-			`${API_URL}/users/${userID}/scrobbles/top/albums?&period=week&limit=6`,
-			'GET',
-			null,
-			cookies
-		),
-		fetcher(
-			`${API_URL}/users/${userID}/scrobbles/top/tracks?period=week&limit=6`,
-			'GET',
-			null,
-			cookies
-		),
-		fetcher(`${API_URL}/users/${userID}/scrobbles/history`, 'GET', null, cookies)
-	]);
+	const overall = getStreamedPeriodData('overall');
+	const year = getStreamedPeriodData('year');
+	const month = getStreamedPeriodData('month');
+	const week = await getStreamedPeriodData('week');
+
+	const history = await fetcher(
+		`${API_URL}/users/${userID}/scrobbles/history`,
+		'GET',
+		null,
+		cookies
+	);
 
 	return {
-		overall: getStreamedPeriodData('overall'),
-		year: getStreamedPeriodData('year'),
-		month: getStreamedPeriodData('month'),
-		week: {
-			artists: wArtists,
-			albums: wAlbums,
-			tracks: wTracks
-		},
+		overall: overall,
+		year: year,
+		month: month,
+		week,
 		history
 	};
 };

d94ef3aa4d7c109996b3c91fee0d1c832d26c36b
RangoDisco <diasmaxime1@gmail.com>
2026-02-13 21:57:27 +0100

    refactor(server): removed metadata service

--- internal/metadata/models/Genre.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Genre struct {
-	ID      int64    `json:"id" gorm:"primary_key"`
-	Name    string   `json:"name"`
-	Artists []Artist `json:"artists" gorm:"many2many:artist_genres;"`
-}

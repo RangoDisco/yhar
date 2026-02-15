78d43b2a41ac5aa85324174bd2798b09ee80aebb
RangoDisco <diasmaxime1@gmail.com>
2026-02-15 17:11:44 +0100

    feat(album): added MusicBrainzID prop

--- internal/api/models/album.go
+++ internal/api/models/album.go
@@ -38,11 +38,12 @@ func (at AlbumType) Value() (driver.Value, error) {
 
 type Album struct {
 	Timestamps
-	ID        int64     `json:"id" gorm:"primary_key;autoIncrement"`
-	Title     string    `json:"title" gorm:"type:varchar(150);not null"`
-	Type      AlbumType `json:"type" gorm:"type:album_type;not null"`
-	Artists   []Artist  `json:"artists" gorm:"many2many:artist_albums;"`
-	Genres    []Genre   `json:"genres" gorm:"many2many:album_genres;"`
-	PictureID int64
-	Picture   Image `json:"picture" gorm:"foreignkey:PictureID"`
+	ID            int64     `json:"id" gorm:"primary_key;autoIncrement"`
+	Title         string    `json:"title" gorm:"type:varchar(150);not null"`
+	Type          AlbumType `json:"type" gorm:"type:album_type;not null"`
+	Artists       []Artist  `json:"artists" gorm:"many2many:artist_albums;"`
+	Genres        []Genre   `json:"genres" gorm:"many2many:album_genres;"`
+	PictureID     int64
+	Picture       Image  `json:"picture" gorm:"foreignkey:PictureID"`
+	MusicBrainzID string `json:"music_brainz_id"`
 }

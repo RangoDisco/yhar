83007449ff45dc68b00e8ae3cbe95e16e6d2d23d
RangoDisco <diasmaxime1@gmail.com>
2026-02-16 07:23:47 +0100

    refactor(server): added generic APIResponse type

--- internal/api/handlers/response.go
+++ internal/api/handlers/response.go
@@ -6,9 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type PaginatedResponse struct {
-	Results    interface{} `json:"results"`
-	Pagination Pagination  `json:"pagination"`
+type APIResponse[T any] struct {
+	Data T `json:"data,omitempty"`
+}
+
+type ErrorResponse struct {
+	Error ErrorMessage `json:"error"`
+}
+
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+
+type PaginatedResponse[T any] struct {
+	Results    T          `json:"results"`
+	Pagination Pagination `json:"pagination"`
 }
 
 type Pagination struct {
@@ -26,20 +38,22 @@ func RespondWithError(c *gin.Context, statusCode int, err error, message string)
 		)
 	}
 
-	c.AbortWithStatusJSON(statusCode, gin.H{"error": message})
+	c.AbortWithStatusJSON(statusCode, ErrorResponse{Error: ErrorMessage{
+		Message: message,
+	}})
 }
 
-func RespondWithData(c *gin.Context, statusCode int, data interface{}) {
-	c.JSON(statusCode, gin.H{"data": data})
+func RespondWithData[T any](c *gin.Context, statusCode int, data T) {
+	c.JSON(statusCode, APIResponse[T]{Data: data})
 }
 
-func BuildPaginatedResponse(results interface{}, page, limit int, totalCount int64) interface{} {
+func BuildPaginatedResponse[T any](results T, page, limit int, totalCount int64) PaginatedResponse[T] {
 	totalPages := int(totalCount) / limit
 	if int(totalCount)%limit > 0 {
 		totalPages++
 	}
 
-	return &PaginatedResponse{
+	return PaginatedResponse[T]{
 		Results: results,
 		Pagination: Pagination{
 			TotalCount:  totalCount,

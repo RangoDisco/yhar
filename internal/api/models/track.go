2b99a4bef3f544223dfdc5b6a912705387506599
RangoDisco <diasmaxime1@gmail.com>
2026-03-09 18:44:31 +0100

    feat(poller): added duration to track model

--- internal/api/models/track.go
+++ internal/api/models/track.go
@@ -1,12 +1,15 @@
 package models
 
+import "time"
+
 type Track struct {
 	Timestamps
 	ID            int64    `json:"id" gorm:"primary_key;autoIncrement"`
 	Title         string   `json:"title" gorm:"type:varchar(150);not null"`
 	Artists       []Artist `json:"artists" gorm:"many2many:track_artists;"`
 	AlbumID       int64
-	Album         Album      `json:"album" gorm:"foreignkey:AlbumID;references:ID;default:null"`
-	MusicBrainzID string     `json:"music_brainz_id" gorm:"type:varchar(255);default:null"`
-	Scrobbles     []Scrobble `json:"scrobbles" gorm:"foreignkey:UserID;"`
+	Album         Album         `json:"album" gorm:"foreignkey:AlbumID;references:ID;default:null"`
+	MusicBrainzID string        `json:"music_brainz_id" gorm:"type:varchar(255);default:null"`
+	Scrobbles     []Scrobble    `json:"scrobbles" gorm:"foreignkey:UserID;"`
+	Duration      time.Duration `json:"duration" gorm:"int"`
 }

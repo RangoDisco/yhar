2c381663aed1b797469660e71cdba20bf3b02f38
RangoDisco <diasmaxime1@gmail.com>
2026-03-09 18:43:09 +0100

    feat(poller): added subsonic implementation

--- internal/api/handlers/scrobble.go
+++ internal/api/handlers/scrobble.go
@@ -1,54 +1,30 @@
 package handlers
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
-	"github.com/rangodisco/yhar/internal/api/models"
-	"github.com/rangodisco/yhar/internal/api/services"
+	"github.com/rangodisco/yhar/internal/api/common"
+	"github.com/rangodisco/yhar/internal/api/pollers"
 )
 
 type ScrobbleHandler struct {
-	service  *services.ScrobbleService
-	subsonic *services.SubsonicService
+	subsonic pollers.PlayerPoller
 }
 
-func NewScrobbleHandler(service *services.ScrobbleService, subsonic *services.SubsonicService) *ScrobbleHandler {
-	return &ScrobbleHandler{service: service, subsonic: subsonic}
+func NewScrobbleHandler(subsonic pollers.PlayerPoller) *ScrobbleHandler {
+	return &ScrobbleHandler{subsonic: subsonic}
 }
 
-// TODO: handle polling and what is considered a real scrobble
 func (h *ScrobbleHandler) ManualNowPlayingPoll(c *gin.Context) {
 	ctx := c.Request.Context()
-	var scrobbles []*models.Scrobble
-	user, exists := c.Get("user")
-	if !exists {
-		c.JSON(500, gin.H{})
-		return
-	}
-	fmt.Println(user)
 
-	subRes, err := h.subsonic.GetNowPlaying()
+	err := h.subsonic.PollPlaying(ctx)
 	if err != nil {
-		c.JSON(500, gin.H{})
+		common.RespondWithError(c, 500, err, "Unable to poll")
 		return
-	}
-
-	if len(subRes.NowPlaying.Entry) == 0 {
-		c.JSON(200, gin.H{})
-		return
-	}
-
-	for _, entry := range subRes.NowPlaying.Entry {
-		// Check if track already exists in service's db
-		res, err := h.service.HandleNewScrobble(ctx, entry)
-		if err != nil {
-			continue
-		}
-		scrobbles = append(scrobbles, res)
 	}
 
 	c.JSON(200, gin.H{
-		"data": scrobbles,
+		"message": "polled subsonic successfully",
 	})
+	return
 }

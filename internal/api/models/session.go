1902992c82ab7b1e8279bfb4580e2087add20d13
RangoDisco <diasmaxime1@gmail.com>
2026-03-09 18:27:03 +0100

    feat(poller): added session model and repo

---
+++ internal/api/models/session.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Session struct {
+	Timestamps
+	ID          int64         `json:"id" gorm:"primary_key;autoIncrement"`
+	PlayerID    string        `json:"player_id" gorm:"varchar(50)"`
+	Username    string        `json:"username" gorm:"varchar(150);not null"`
+	Title       string        `json:"title" gorm:"varchar(150);not null"`
+	Artist      string        `json:"artist" gorm:"varchar(150)"`
+	Album       string        `json:"album" gorm:"varchar(150)"`
+	Duration    time.Duration `json:"duration" gorm:"not null"`
+	StartedAt   time.Time     `json:"started_at" gorm:"not null"`
+	LastSeenAt  time.Time     `json:"last_seen_at" gorm:"not null"`
+	CompletedAt time.Time     `json:"completed_at" gorm:"default"`
+}
---
+++ internal/api/repositories/session.go
@@ -0,0 +1,57 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rangodisco/yhar/internal/api/models"
+	"github.com/rangodisco/yhar/internal/api/types/filters"
+	"gorm.io/gorm"
+)
+
+type SessionRepository struct {
+	Db *gorm.DB
+}
+
+func NewSessionRepository(Db *gorm.DB) *SessionRepository {
+	return &SessionRepository{
+		Db: Db,
+	}
+}
+
+func (r *SessionRepository) FindByFilters(ctx context.Context, filters []filters.QueryFilter) (*models.Session, error) {
+	var s models.Session
+	query := r.Db.WithContext(ctx).
+		Where("completed_at IS null")
+
+	for _, filter := range filters {
+		query.Where(filter.Key+" = ?", filter.Value)
+	}
+
+	err := query.First(&s).Error
+	if err != nil {
+		return nil, fmt.Errorf("unable to find session: %w", err)
+	}
+
+	return &s, nil
+}
+
+func (r *SessionRepository) Persist(ctx context.Context, s *models.Session) error {
+	err := r.Db.WithContext(ctx).Create(&s).Error
+
+	if err != nil {
+		return fmt.Errorf("unable to persist session: %w", err)
+	}
+
+	return nil
+}
+
+func (r *SessionRepository) Update(ctx context.Context, s *models.Session) error {
+	err := r.Db.WithContext(ctx).Save(&s).Error
+
+	if err != nil {
+		return fmt.Errorf("unable to update session: %w", err)
+	}
+
+	return nil
+}

6c4b12c36070d545343e4d774b1bc25cfc30b4e1
RangoDisco <diasmaxime1@gmail.com>
2026-03-13 06:37:42 +0100

    feat(providers): added deezer provider

---
+++ internal/api/providers/deezer.go
@@ -0,0 +1,60 @@
+package providers
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+)
+
+type DeezerProvider struct {
+	name    string
+	baseURL string
+}
+
+type ArtistRes struct {
+	Data []Artist `json:"data"`
+}
+
+type Artist struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	PictureSmall string `json:"picture_small"`
+}
+
+func NewDeezerProvider() MetadataProvider {
+	return &DeezerProvider{
+		name:    "deezer",
+		baseURL: "https://api.deezer.com/search",
+	}
+}
+
+func (d *DeezerProvider) Name() string {
+	return d.name
+}
+
+func (d *DeezerProvider) GetTrackByInfos(ctx context.Context, infos ScrobbleData) (*TrackMetadata, error) {
+
+	return nil, fmt.Errorf("method is not implemented")
+}
+
+func (d *DeezerProvider) GetArtistImage(ctx context.Context, _, name string) (string, error) {
+	params := url.Values{
+		"q": {name},
+	}
+
+	endpoint := fmt.Sprintf("%s/%s", d.baseURL, "artist")
+
+	var artistRes ArtistRes
+	err := sendRequest(ctx, endpoint, nil, nil, params, &artistRes)
+	if err != nil {
+		return "", fmt.Errorf("unable to query artist: %w", err)
+	}
+
+	if artistRes.Data == nil || len(artistRes.Data) == 0 {
+		return "", fmt.Errorf("no artist found")
+	}
+
+	artist := artistRes.Data[0]
+
+	return artist.PictureSmall, nil
+}

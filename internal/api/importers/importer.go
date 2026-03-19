9a516ee731a3257d24bdb1da49e29b18485accf3
RangoDisco <diasmaxime1@gmail.com>
2026-03-19 08:37:07 +0100

    fix(importers): removed unexported function from interface

--- internal/api/importers/importer.go
+++ internal/api/importers/importer.go
@@ -2,12 +2,9 @@ package importers
 
 import (
 	"context"
-
-	"github.com/rangodisco/yhar/internal/api/services"
 )
 
 type Importer interface {
 	Name() string
 	Import(ctx context.Context) error
-	parseToUnifiedScrobble(ctx context.Context, entry interface{}) (*services.UnifiedScrobbleEntry, error)
 }

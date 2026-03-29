f564cbc7ed23a7645839d07cb1917d40710accd1
RangoDisco <diasmaxime1@gmail.com>
2026-03-29 18:21:00 +0200

    feat(auth): handled automatic redirection to profile or login page

---
+++ web/src/routes/+page.server.ts
@@ -0,0 +1,17 @@
+import type { PageServerLoad } from './$types';
+import { fetcher } from '$lib/fetcher';
+import { API_URL } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async ({ cookies }) => {
+	if (cookies.get('token')) {
+		// Get current user and redirect to their profile
+		const response = await fetcher(`${API_URL}/users/me`, 'GET', cookies, null);
+		const userID = response.user.id;
+
+		redirect(302, `/users/${userID}/top`);
+	}
+
+	// Otherwise, redirect to login
+	redirect(302, `/auth/login`);
+};

73bc76c8fe81d597e0f14147bfadbae30cd10058
RangoDisco <diasmaxime1@gmail.com>
2026-03-30 08:03:14 +0200

    feat(logs): reworked access/error logging to use new slog default logger

--- internal/api/middlewares/logger.go
+++ internal/api/middlewares/logger.go
@@ -3,29 +3,41 @@ package middlewares
 import (
 	"log/slog"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rangodisco/yhar/internal/api/dto"
 )
 
-func LoggerMiddleware(l *slog.Logger) gin.HandlerFunc {
+func LoggerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		logger := l.With(
+		startTime := time.Now()
+
+		c.Next()
+
+		args := []any{
 			slog.String("method", c.Request.Method),
 			slog.String("path", c.Request.URL.Path),
 			slog.String("ip", c.ClientIP()),
-		)
+			slog.String("duration", time.Since(startTime).String()),
+			slog.Int("status", c.Writer.Status()),
+		}
 
+		// User might not be set, in case of a public route
 		rawUser, exists := c.Get("user")
 		if exists && rawUser != nil {
 			user, ok := rawUser.(*dto.UserPassport)
 			if ok {
 				id := strconv.FormatInt(user.ID, 10)
-				logger = logger.With(slog.String("user_id", id))
+				args = append(args, slog.String("user_id", id))
 			}
 		}
 
-		c.Set("logger", logger)
-		c.Next()
+		if len(c.Errors) > 0 {
+			args = append(args, slog.String("error", c.Errors.Last().Error()))
+			slog.Error("Request completed with errors", args...)
+		} else {
+			slog.Info("Request completed", args...)
+		}
 	}
 }

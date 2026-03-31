d1c72935d1efbe54b9d93c34b9385e4ea2fd343d
RangoDisco <diasmaxime1@gmail.com>
2026-03-31 19:30:48 +0200

    feat(tracks): handled new tracks stats full page

---
+++ web/src/routes/users/[userID]/top/tracks/+page.server.ts
@@ -0,0 +1,22 @@
+import type { PageServerLoad } from './$types';
+import { fetcher } from '$lib/fetcher';
+import { API_URL } from '$env/static/private';
+
+export const load: PageServerLoad = async ({ url, params, cookies }) => {
+	const { userID } = params;
+	const page = url.searchParams.get('page') ?? "1";
+	const period = url.searchParams.get('period') ?? 'week';
+	const artist = url.searchParams.get('artist') ?? '';
+
+	const tracks = await fetcher(
+		`${API_URL}/users/${userID}/scrobbles/top/tracks?period=${period}&page=${page}&artist=${artist}&limit=10`,
+		'GET',
+		cookies,
+		null
+	);
+
+	return {
+		period,
+		tracks,
+	};
+};

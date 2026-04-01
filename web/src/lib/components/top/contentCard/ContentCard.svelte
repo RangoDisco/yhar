55e5538bf8a90de75313832902e55337ed5730c6
RangoDisco <diasmaxime1@gmail.com>
2026-04-01 17:22:03 +0200

    style(content): updated content card title's max lines

--- web/src/lib/components/top/contentCard/ContentCard.svelte
+++ web/src/lib/components/top/contentCard/ContentCard.svelte
@@ -14,6 +14,6 @@
 <article class="w-[30%] md:w-[15%] flex flex-col items-center justify-center gap-2">
     <ContentCardWrapper {contentID} {contentType}>
         <ContentCardImage {contentType} {contentID} {pictureUrl}/>
-        <p class="text-center mb-auto">{title}</p>
+        <p class="text-center mb-auto h-12 line-clamp-2">{title}</p>
     </ContentCardWrapper>
 </article>

7ae49c765361d77c6caeaf724c8c1b146754a235
RangoDisco <diasmaxime1@gmail.com>
2026-04-01 17:21:34 +0200

    feat(history): added full page stats

---
+++ web/src/routes/users/[userID]/history/+page.server.ts
@@ -0,0 +1,20 @@
+import type { PageServerLoad } from './$types';
+import { API_URL } from '$env/static/private';
+import { fetcher } from '$lib/fetcher';
+
+export const load: PageServerLoad = async ({ url, params, cookies }) => {
+	const { userID } = params;
+	const page = url.searchParams.get('page') ?? 1;
+	const artist = url.searchParams.get('artist') ?? '';
+
+	const history = await fetcher(
+		`${API_URL}/users/${userID}/scrobbles/history?artist=${artist}&page=${page}&limit=20`,
+		'GET',
+		cookies,
+		null
+	);
+
+	return {
+		history
+	};
+};

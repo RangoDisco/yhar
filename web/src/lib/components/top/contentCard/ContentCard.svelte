cc195a0edbde97ac22be6ab63788bc20d10d9a02
RangoDisco <diasmaxime1@gmail.com>
2026-04-02 20:20:15 +0200

    fix(content): removed now useless contentID prop

--- web/src/lib/components/top/contentCard/ContentCard.svelte
+++ web/src/lib/components/top/contentCard/ContentCard.svelte
@@ -13,7 +13,7 @@
 
 <article class="w-[30%] md:w-[15%] flex flex-col items-center justify-center gap-2">
     <ContentCardWrapper {contentID} {contentType}>
-        <ContentCardImage {contentType} {contentID} {pictureUrl}/>
+        <ContentCardImage {contentType} {pictureUrl}/>
         <p class="text-center mb-auto h-12 line-clamp-2">{title}</p>
     </ContentCardWrapper>
 </article>

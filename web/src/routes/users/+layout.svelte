f216f0b8d5d529f4d498b882cf5b300d1768bce6
RangoDisco <diasmaxime1@gmail.com>
2026-04-04 10:04:15 +0200

    feat(auth): added logout button when logged in

---
+++ web/src/routes/users/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import {Button} from "$lib/components/ui/button";
+
+    let {children, data} = $props();
+</script>
+
+<main class="flex flex-col gap-4">
+    {#if data.isLoggedIn}
+        <div class="flex flex-col gap-2">
+            <nav class="w-full">
+                <ul class="flex w-full flex-wrap items-center justify-end gap-2">
+                    <li>
+                        <form method="POST" action="/auth/logout">
+                            <Button size="sm" type="submit">Logout</Button>
+                        </form>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    {/if}
+    {@render children()}
+</main>

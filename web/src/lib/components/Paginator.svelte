c590dffabc11527218ffa86132bdda982720b9d1
RangoDisco <diasmaxime1@gmail.com>
2026-04-06 13:13:39 +0200

    feat(pagination): added custom Paginator component

---
+++ web/src/lib/components/Paginator.svelte
@@ -0,0 +1,34 @@
+<script>
+    import * as Pagination from "$lib/components/ui/pagination/index";
+    import {handlePageChange, PER_PAGE} from "$lib/pagination";
+
+    let {totalCount, page} = $props();
+
+</script>
+
+<Pagination.Root count={totalCount} perPage={PER_PAGE} page={page}
+                 onPageChange={handlePageChange}>
+    {#snippet children({pages, currentPage})}
+        <Pagination.Content>
+            <Pagination.Item>
+                <Pagination.Previous/>
+            </Pagination.Item>
+            {#each pages as page (page.key)}
+                {#if page.type === "ellipsis"}
+                    <Pagination.Item>
+                        <Pagination.Ellipsis/>
+                    </Pagination.Item>
+                {:else}
+                    <Pagination.Item>
+                        <Pagination.Link {page} isActive={currentPage === page.value}>
+                            {page.value}
+                        </Pagination.Link>
+                    </Pagination.Item>
+                {/if}
+            {/each}
+            <Pagination.Item>
+                <Pagination.Next/>
+            </Pagination.Item>
+        </Pagination.Content>
+    {/snippet}
+</Pagination.Root>

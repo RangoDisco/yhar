8a5fbf82fd5e06ad817702e75b86a16de7590334
RangoDisco <diasmaxime1@gmail.com>
2026-04-06 16:08:11 +0200

    chore: used Period enum instead of string when defining available periods

--- web/src/routes/users/[userID]/top/+page.svelte
+++ web/src/routes/users/[userID]/top/+page.svelte
@@ -1,12 +1,13 @@
-<script>
+<script lang="ts">
     import * as Tabs from "$lib/components/ui/tabs/index";
     import ContentListItem from "$lib/components/top/ContentListItem.svelte";
     import ContentListWrapper from "$lib/components/top/ContentListWrapper.svelte";
     import HistoryListItem from "$lib/components/top/tracks/HistoryListItem.svelte";
     import ContentCard from "$lib/components/top/contentCard/ContentCard.svelte";
+    import {Period} from "$lib/types/period"
 
     const {data} = $props();
-    const periods = ['week', 'month', 'year', 'overall'];
+    const periods = [Period.week, Period.month, Period.year, Period.overall];
 </script>
 <div class="flex flex-col gap-8">
     <Tabs.Root value="week">
@@ -23,7 +24,7 @@
                     <!--TOP ARTISTS-->
                     <ContentListWrapper title="Top artists" url="top/artists">
                         <div class="flex flex-wrap gap-4">
-                            {#each periodData.artists.results as artist, i (artist.id)}
+                            {#each periodData?.artists.results as artist, i (artist.id)}
                                 <ContentCard title={artist.name} pictureUrl={artist.picture_url} contentType="artists"
                                              contentID={artist.id}/>
                             {/each}
@@ -33,7 +34,7 @@
                     <!--TOP ALBUMS-->
                     <ContentListWrapper title="Top albums" url="top/albums">
                         <div class="flex flex-wrap gap-4">
-                            {#each periodData.albums.results as album, i (album.id)}
+                            {#each periodData?.albums.results as album, i (album.id)}
                                 <ContentCard title={album.title} pictureUrl={album.picture_url}
                                              contentID={album.id}
                                              contentType="albums"/>
@@ -44,7 +45,7 @@
                     <!--TOP TRACKS-->
                     <ContentListWrapper title="Top tracks" url="top/tracks">
                         <div class="flex flex-col gap-4">
-                            {#each periodData.tracks.results as track, i (track.id)}
+                            {#each periodData?.tracks.results as track, i (track.id)}
                                 <ContentListItem index={i+1} title={track.title}
                                                  pictureUrl={track.picture_url}
                                                  scrobbleCount={track.scrobble_count}

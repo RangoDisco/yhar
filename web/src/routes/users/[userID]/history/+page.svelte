e01427f8d0b5b09ad5a8531aa7919dbcc3e2710a
RangoDisco <diasmaxime1@gmail.com>
2026-04-06 16:08:56 +0200

    fix(history): replacement pagination with new Paginator component

--- web/src/routes/users/[userID]/history/+page.svelte
+++ web/src/routes/users/[userID]/history/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
     import * as Pagination from "$lib/components/ui/pagination/index";
-    import {goto} from "$app/navigation";
     import HistoryListItem from "$lib/components/top/tracks/HistoryListItem.svelte";
     import ContentListWrapper from "$lib/components/top/ContentListWrapper.svelte";
-    import {page} from "$app/state";
+    import Paginator from "$lib/components/Paginator.svelte";
 
     let {data} = $props();
 
-    const handlePageChange = (newPage: number) => {
-        const query = new URLSearchParams(page.url.searchParams.toString());
-        query.set("page", newPage.toString());
-        goto(`?${query.toString()}`, {keepFocus: true});
-    };
+    let currentPage = $derived(parseInt(data.page));
 
 </script>
 <ContentListWrapper title="History">
@@ -21,29 +16,4 @@
         {/each}
     </div>
 </ContentListWrapper>
-<Pagination.Root count={data.history.pagination.total_count} perPage={10} page={data.page}
-                 onPageChange={handlePageChange}>
-    {#snippet children({pages, currentPage})}
-        <Pagination.Content>
-            <Pagination.Item>
-                <Pagination.Previous/>
-            </Pagination.Item>
-            {#each pages as page (page.key)}
-                {#if page.type === "ellipsis"}
-                    <Pagination.Item>
-                        <Pagination.Ellipsis/>
-                    </Pagination.Item>
-                {:else}
-                    <Pagination.Item>
-                        <Pagination.Link {page} isActive={currentPage === page.value}>
-                            {page.value}
-                        </Pagination.Link>
-                    </Pagination.Item>
-                {/if}
-            {/each}
-            <Pagination.Item>
-                <Pagination.Next/>
-            </Pagination.Item>
-        </Pagination.Content>
-    {/snippet}
-</Pagination.Root>
+<Paginator totalCount={data.history.pagination.total_count} page={currentPage}/>

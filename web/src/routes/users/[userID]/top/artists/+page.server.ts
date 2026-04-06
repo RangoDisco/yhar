9e362cdbf9f82568ac3ecbcc8fd8cd7be86255ad
RangoDisco <diasmaxime1@gmail.com>
2026-04-06 16:07:42 +0200

    chore: used new types in all server functions

--- web/src/routes/users/[userID]/top/artists/+page.server.ts
+++ web/src/routes/users/[userID]/top/artists/+page.server.ts
@@ -1,13 +1,15 @@
 import type { PageServerLoad } from './$types';
 import { fetcher } from '$lib/fetcher';
 import { API_URL } from '$env/static/private';
+import type { Paginated } from '$lib/types/pagination';
+import type { Artist } from '$lib/types/content';
 
 export const load: PageServerLoad = async ({ url, params, cookies }) => {
 	const { userID } = params;
 	const page = url.searchParams.get('page') ?? '1';
 	const period = url.searchParams.get('period') ?? 'week';
 
-	const artists = await fetcher(
+	const artists: Paginated<Artist> = await fetcher(
 		`${API_URL}/users/${userID}/scrobbles/top/artists?period=${period}&page=${page}&limit=10`,
 		'GET',
 		cookies,
--- web/src/routes/users/[userID]/top/tracks/+page.server.ts
+++ web/src/routes/users/[userID]/top/tracks/+page.server.ts
@@ -1,14 +1,16 @@
 import type { PageServerLoad } from './$types';
 import { fetcher } from '$lib/fetcher';
 import { API_URL } from '$env/static/private';
+import type { Paginated } from '$lib/types/pagination';
+import type { Track } from '$lib/types/content';
 
 export const load: PageServerLoad = async ({ url, params, cookies }) => {
 	const { userID } = params;
-	const page = url.searchParams.get('page') ?? "1";
+	const page = url.searchParams.get('page') ?? '1';
 	const period = url.searchParams.get('period') ?? 'week';
 	const artist = url.searchParams.get('artist') ?? '';
 
-	const tracks = await fetcher(
+	const tracks: Paginated<Track> = await fetcher(
 		`${API_URL}/users/${userID}/scrobbles/top/tracks?period=${period}&page=${page}&artist=${artist}&limit=10`,
 		'GET',
 		cookies,

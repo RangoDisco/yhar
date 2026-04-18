6c59d91fb361fa4b1d3c3e0e1b731458151cb85e
RangoDisco <diasmaxime1@gmail.com>
2026-04-18 13:37:57 +0200

    fix(auth): added user in locals type

--- web/src/app.d.ts
+++ web/src/app.d.ts
@@ -3,7 +3,14 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: {
+				id: string;
+				username: string;
+				role: 'USER' | 'ADMIN';
+				expiresAt: number;
+			};
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}

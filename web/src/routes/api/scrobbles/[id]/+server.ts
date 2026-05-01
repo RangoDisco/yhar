d2797f64943b837fbad158c04af88853a81f53a6
RangoDisco <diasmaxime1@gmail.com>
2026-05-01 12:12:27 +0200

    feat(scrobble): handled scrobble removal

---
+++ web/src/routes/api/scrobbles/[id]/+server.ts
@@ -0,0 +1,12 @@
+import { fetcher } from '$lib/fetcher';
+import { API_URL } from '$env/static/private';
+import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
+
+export const DELETE: RequestHandler = async ({ params, cookies }) => {
+	const { id } = params;
+
+	const res = await fetcher(`${API_URL}/scrobbles/${id}`, 'DELETE', cookies);
+
+	return json({ success: true });
+};

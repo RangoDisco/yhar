31bc8a59b03089c1e0c6c71f08f49a79e260f0ee
RangoDisco <diasmaxime1@gmail.com>
2026-05-23 20:58:51 +0200

    fix(sessions): prevented service from finding unfinished/stale session when polling

--- internal/api/repositories/session.go
+++ internal/api/repositories/session.go
@@ -1,7 +1,10 @@
 package repositories
 
 import (
+	"context"
+
 	"github.com/rangodisco/yhar/internal/api/models"
+	"github.com/rangodisco/yhar/internal/api/types/subsonic"
 	"gorm.io/gorm"
 )
 
@@ -13,4 +16,19 @@ func NewSessionRepository(Db *gorm.DB) *SessionRepository {
 	return &SessionRepository{
 		BaseRepository[models.Session]{Db: Db, Table: "sessions"},
 	}
+}
+
+// FindCurrentByEntry find recent/active sessions for a given combo of user + track
+func (r *SessionRepository) FindCurrentByEntry(ctx context.Context, entry subsonic.Entry) (*models.Session, error) {
+	res, err := gorm.G[models.Session](r.Db).
+		Where("username = ?", entry.Username).
+		Where("player_id = ?", entry.PlayerID).
+		Where("title = ?", entry.Title).
+		Where("completed_at IS null").
+		Where("EXTRACT(EPOCH FROM (NOW() - created_at)) <= ?", entry.Duration).First(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }

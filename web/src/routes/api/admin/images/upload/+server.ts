168c52a54327d829bc3a68196cd222a6633aa2dd
RangoDisco <diasmaxime1@gmail.com>
2026-06-13 11:03:32 +0200

    fix(images): updated .gitignore file to exclude images api route

---
+++ web/src/routes/api/admin/images/upload/+server.ts
@@ -0,0 +1,27 @@
+import { fetcher } from '$lib/fetcher';
+const API_URL = process.env.API_URL
+import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+	const formData = await request.formData();
+	const image = formData.get('image') as File;
+	const contentID = formData.get('contentID');
+	const contentType = formData.get('contentType');
+
+	const uploadFormData = new FormData();
+	uploadFormData.append('image', image);
+
+	const uploadRes = await fetcher(`${API_URL}/images`, 'POST', cookies, uploadFormData);
+
+	const updateRes = await fetcher(
+		`${API_URL}/${contentType}/${contentID}`,
+		'PATCH',
+		cookies,
+		JSON.stringify({
+			image_id: uploadRes.id
+		})
+	);
+
+	return json({ success: true });
+};

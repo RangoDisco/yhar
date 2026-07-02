c7cf1e8a66eed6fea46fa819df6a52a6a2a4997e
RangoDisco <diasmaxime1@gmail.com>
2026-07-02 18:46:44 +0200

    feat(nav): added home btn + basic style

---
+++ web/src/lib/components/Nav.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+
+    import {Button} from "$lib/components/ui/button";
+    import ModeToggler from "$lib/components/ModeToggler.svelte";
+
+    type Props = {
+        user: {
+            id: string
+            username: string
+            role: "USER" | "ADMIN"
+            expiresAt: number
+        }
+    }
+
+    let {user}: Props = $props();
+
+</script>
+
+<nav class="w-full">
+    <div class="flex w-full flex-wrap items-center justify-between gap-2">
+        <Button href="/" variant="ghost" size="sm">Home</Button>
+        <div class="flex items-center gap-2">
+            {#if user}
+                <div>
+                    <form method="POST" action="/auth/logout">
+                        <Button size="sm" type="submit">Logout</Button>
+                    </form>
+                </div>
+            {/if}
+            <div>
+                <ModeToggler/>
+            </div>
+        </div>
+    </div>
+</nav>

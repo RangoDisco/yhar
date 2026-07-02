24d34e79639b8294ee726e72593f7b8ff58a67c1
RangoDisco <diasmaxime1@gmail.com>
2026-07-02 18:46:44 +0200

    feat(nav): added home btn + basic style

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
     import "./layout.css";
-    import favicon from "$lib/assets/favicon.svg";
     import {Toaster} from "$lib/components/ui/sonner";
-
     import {ModeWatcher} from "mode-watcher";
 
     let {children} = $props();
 </script>
 
-<section class="min-h-screen md:max-w-300 mx-auto p-6">
+<section class="min-h-screen">
     <ModeWatcher/>
     <Toaster/>
     {@render children()}
--- web/src/routes/users/+layout.svelte
+++ web/src/routes/users/+layout.svelte
@@ -1,26 +1,17 @@
 <script lang="ts">
     import {Button} from "$lib/components/ui/button";
     import ModeToggler from "$lib/components/ModeToggler.svelte";
+    import Nav from "$lib/components/Nav.svelte";
 
     let {children, data} = $props();
 </script>
 
-<main class="flex flex-col gap-4">
-    <div class="flex flex-col gap-2">
-        <nav class="w-full">
-            <ul class="flex w-full flex-wrap items-center justify-end gap-2">
-                {#if data.user}
-                    <li>
-                        <form method="POST" action="/auth/logout">
-                            <Button size="sm" type="submit">Logout</Button>
-                        </form>
-                    </li>
-                {/if}
-                <li>
-                    <ModeToggler/>
-                </li>
-            </ul>
-        </nav>
+<main class="flex flex-col">
+    <div class="flex items-center gap-2 px-6 h-17">
+        <Nav user={data.user}/>
     </div>
-    {@render children()}
+    <hr/>
+    <div class="md:max-w-300 w-full mx-auto p-6">
+        {@render children()}
+    </div>
 </main>
